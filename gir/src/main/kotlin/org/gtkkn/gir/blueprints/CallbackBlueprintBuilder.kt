f91fe91e1cd33f934618107db7ae2b905d930c2f
Steven Van Bael <steven@quantus.io>
2023-03-12 17:17:14 +0100

    gir: Add Callback blueprint stub

---
+++ gir/src/main/kotlin/org/gtkkn/gir/blueprints/CallbackBlueprintBuilder.kt
@@ -0,0 +1,20 @@
+package org.gtkkn.gir.blueprints
+
+import org.gtkkn.gir.model.GirCallback
+import org.gtkkn.gir.model.GirNamespace
+import org.gtkkn.gir.processor.ProcessorContext
+
+class CallbackBlueprintBuilder(
+    context: ProcessorContext,
+    private val girNamespace: GirNamespace,
+    private val girCallback: GirCallback
+) : BlueprintBuilder<CallbackBlueprint>(context) {
+    override fun blueprintObjectType(): String = "callback"
+
+    override fun blueprintObjectName(): String = girCallback.name
+
+    override fun build(): BlueprintResult<CallbackBlueprint> {
+        return skip("Callbacks are not supported")
+    }
+
+}
