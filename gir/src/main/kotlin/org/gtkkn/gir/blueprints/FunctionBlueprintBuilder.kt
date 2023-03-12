8a741c6e15a6fecd8e0f7bd08d4073df8cfb0238
Steven Van Bael <steven@quantus.io>
2023-03-12 16:48:22 +0100

    gir: Add Function blueprint stub

---
+++ gir/src/main/kotlin/org/gtkkn/gir/blueprints/FunctionBlueprintBuilder.kt
@@ -0,0 +1,17 @@
+package org.gtkkn.gir.blueprints
+
+import org.gtkkn.gir.model.GirFunction
+import org.gtkkn.gir.model.GirNamespace
+import org.gtkkn.gir.processor.ProcessorContext
+
+class FunctionBlueprintBuilder(
+    context: ProcessorContext,
+    private val girNamespace: GirNamespace,
+    private val girFunction: GirFunction
+) : BlueprintBuilder<FunctionBlueprint>(context) {
+    override fun blueprintObjectType(): String = "function"
+    override fun blueprintObjectName(): String = girFunction.name
+
+    override fun build(): BlueprintResult<FunctionBlueprint> = skip("Functions not supported")
+
+}
