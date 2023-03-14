2c2568422ddb3353f4e556fb4188c34e92e762ae
Steven Van Bael <steven@quantus.io>
2023-03-14 21:29:23 +0100

    bindings: Add bindings modules for gtk and its dependencies

--- samples/gtk/hello-world/src/nativeMain/kotlin/org/gtkkn/samples/gtk/helloworld/Main.kt
+++ samples/gtk/hello-world/src/nativeMain/kotlin/org/gtkkn/samples/gtk/helloworld/Main.kt
@@ -1,5 +1,10 @@
 package org.gtkkn.samples.gtk.helloworld
 
+import native.gio.g_application_new
+import native.gio.g_application_run
+
 fun main() {
     println("Hello World!")
+    val gApp = g_application_new("org.gtkkn.samples.gtk.helloworld", 0)!!
+    g_application_run(gApp, 0, null)
 }
