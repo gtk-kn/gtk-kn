190a941a2a9784bd3c48bf053cb065781025b7d2
Steven Van Bael <steven@quantus.io>
2023-03-25 19:27:06 +0100

    bindings: Move staticStableRefDestroy function from gobject to glib

--- bindings/core/glib/src/nativeMain/kotlin/org/gtkkn/common/StableRef.kt
+++ bindings/core/glib/src/nativeMain/kotlin/org/gtkkn/common/StableRef.kt
@@ -1,19 +1,22 @@
 package org.gtkkn.common
 
+import kotlinx.cinterop.CFunction
 import kotlinx.cinterop.COpaquePointer
+import kotlinx.cinterop.CPointed
+import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.asStableRef
 import kotlinx.cinterop.reinterpret
 import kotlinx.cinterop.staticCFunction
-import native.gobject.GClosureNotify
+import native.glib.gpointer
 
 /**
  * Helper function that can be used as a destroy_data handler
- * when connecting to a signal.
+ * when connecting to a signal or when executing callbacks.
  *
  * This implementation assumes [data] is a pointer to a StableRef
  * and calls dispose on it.
  */
-public val staticStableRefDestroy: GClosureNotify =
+public val staticStableRefDestroy: CPointer<CFunction<(gpointer?, CPointer<CPointed>?) -> Unit>> =
     staticCFunction { data: COpaquePointer?,
             _: COpaquePointer?, ->
         data?.asStableRef<Any>()?.dispose()
