e93036bf53dfecde7b707c7f5b2bf1a1a6aeb91f
Roberto Leinardi <leinardi@gmail.com>
2023-04-21 10:20:11 +0200

    licenses: Fix typo

--- gir/src/main/kotlin/org/gtkkn/gir/model/GirAnyType.kt
+++ gir/src/main/kotlin/org/gtkkn/gir/model/GirAnyType.kt
@@ -1,17 +1,17 @@
 /*
- * Copyright (c) 2023 gtk-kt
+ * Copyright (c) 2023 gtk-kn
  *
- * This file is part of gtk-kt.
- * gtk-kt is free software: you can redistribute it and/or modify
+ * This file is part of gtk-kn.
+ * gtk-kn is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * gtk-kt is distributed in the hope that it will be useful,
+ * gtk-kn is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  * You should have received a copy of the GNU General Public License
- * along with gtk-kt. If not, see https://www.gnu.org/licenses/.
+ * along with gtk-kn. If not, see https://www.gnu.org/licenses/.
  */
 
 package org.gtkkn.gir.model
